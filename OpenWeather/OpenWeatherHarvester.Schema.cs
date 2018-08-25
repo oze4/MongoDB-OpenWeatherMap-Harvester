73a58191639f2f3aa36bffe1e23c78966947bca6
moestreich <moestreich@centretechnologies.com>
2018-08-25 18:42:24 -0500

    combined classes again because I am not a smart man

--- OpenWeather/OpenWeatherHarvester.Schema.cs
+++ OpenWeather/OpenWeatherHarvester.Schema.cs
@@ -56,9 +56,9 @@ namespace OpenWeatherHarvester.Schema
         {
             var baseIconUrl = string.Format("https://openweathermap.org/img/w"); // set icon
             var iconCode = json["weather"][0]["icon"].Value<string>(); // set icon
-
-            timestamp = DateTime.Now;
+            //
             dateTime = TimeConverter.UtcToLocal(json["dt"].Value<double>());
+            //
             weather_id = json["id"].Value<string>();
             city = json["name"].Value<string>();
             code = json["cod"].Value<int>();
@@ -102,5 +102,130 @@ namespace OpenWeatherHarvester.Schema
                 json["coord"]["lat"].Value<int>()
                 );
         }
-    }    
+    }
+
+    [BsonIgnoreExtraElements]
+    internal class Weather
+    {
+        [BsonElement(elementName: "Id")]
+        internal string Id { get; set; }
+
+        [BsonElement(elementName: "Main")]
+        internal string Main { get; set; }
+
+        [BsonElement(elementName: "Description")]
+        internal string Description { get; set; }
+
+        [BsonElement(elementName: "Icon")]
+        internal string Icon { get; set; }
+
+        internal Weather(string id, string main, string description, string icon)
+        {
+            Id = id;
+            Main = main;
+            Description = description;
+            Icon = icon;
+        }
+    }
+
+    [BsonIgnoreExtraElements]
+    internal class Sys
+    {
+        [BsonElement(elementName: "Type")]
+        internal int Type { get; set; }
+
+        [BsonElement(elementName: "Id")]
+        internal int Id { get; set; }
+
+        [BsonElement(elementName: "Message")]
+        internal int Message { get; set; }
+
+        [BsonElement(elementName: "Country")]
+        internal string Country { get; set; }
+
+        [BsonElement(elementName: "Sunrise")]
+        internal DateTime Sunrise { get; set; }
+
+        [BsonElement(elementName: "Sunset")]
+        internal DateTime Sunset { get; set; }
+
+        internal Sys(int type, int id, int message, string country, DateTime sunrise, DateTime sunset)
+        {
+            Type = type;
+            Id = id;
+            Message = message;
+            Country = country;
+            Sunrise = sunrise;
+            Sunset = sunset;
+        }
+    }
+
+    [BsonIgnoreExtraElements]
+    internal class Main
+    {
+        [BsonElement(elementName: "Temp")]
+        internal float Temp { get; set; }
+
+        [BsonElement(elementName: "Pressure")]
+        internal float Pressure { get; set; }
+
+        [BsonElement(elementName: "Humidity")]
+        internal float Humidity { get; set; }
+
+        [BsonElement(elementName: "Temp_Min")]
+        internal float Temp_Min { get; set; }
+
+        [BsonElement(elementName: "Temp_Max")]
+        internal float Temp_Max { get; set; }
+
+        internal Main(float temp, float pressure, float humidity, float temp_min, float temp_max)
+        {
+            Temp = temp;
+            Pressure = pressure;
+            Humidity = humidity;
+            Temp_Max = temp_max;
+            Temp_Min = temp_min;
+        }
+    }
+
+    [BsonIgnoreExtraElements]
+    internal class Coordinate
+    {
+        [BsonElement(elementName: "Longitude")]
+        internal int Longitude { get; set; }
+
+        [BsonElement(elementName: "Latitude")]
+        internal int Latitude { get; set; }
+
+        internal Coordinate(int lon, int lat)
+        {
+            Longitude = lon;
+            Latitude = lat;
+        }
+    }
+
+    [BsonIgnoreExtraElements]
+    internal class Clouds
+    {
+        [BsonElement(elementName: "All")]
+        internal int All { get; set; }
+
+        internal Clouds(int all)
+        {
+            All = all;
+        }
+    }
+
+    [BsonIgnoreExtraElements]
+    internal class Wind
+    {
+        [BsonElement(elementName: "Speed")]
+        internal int Speed { get; set; }
+
+        internal Wind(int speed)
+        {
+            Speed = speed;
+        }
+    }
+
 }
