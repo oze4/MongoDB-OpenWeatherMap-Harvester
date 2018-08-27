785c7dec52b3f52eca397e71365d107c45a54ff7
moestreich <moestreich@centretechnologies.com>
2018-08-26 19:49:12 -0500

    formatting

--- OpenWeather/OpenWeatherHarvester.Schema.Cities.cs
+++ OpenWeather/OpenWeatherHarvester.Schema.Cities.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 // ~ UNDER CONSTRUCTION, BUILDING ENUM WITH LIKE 200K CITIES ~
 
